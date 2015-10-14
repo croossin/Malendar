1a7b94b74163fb5af17e8e458ffd4b1d19a816f0
croossin <croossin94@gmail.com>
2015-10-14 15:19:41 -0500

    Initial Commit

---
+++ Malendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Malendar
+//
+//  Created by Chase Roossin on 10/14/15.
+//  Copyright © 2015 Smart Drive LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
